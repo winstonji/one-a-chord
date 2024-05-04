0e6a709ac239f3675b9e7d4897a37755d251b4e7
winstonji <winston.wy.ji@gmail.com>
2024-05-03 22:56:48 -0600

    temp remove electron call

--- src/react/view/home.tsx
+++ src/react/view/home.tsx
@@ -1,19 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import '../styles/global.scss'
 const Home = () => {
-	const [backendData, setBackendData] = useState<string | undefined>()
-	useEffect(() => {
-		(window as any).electron.ipcRenderer
-			.invoke('exampleHandler')
-			.then((data: string) => {
-				setBackendData(data)
-			})
-	}, [])
+	// const [backendData, setBackendData] = useState<string | undefined>()
+	// useEffect(() => {
+	// 	(window as any).electron.ipcRenderer
+	// 		.invoke('exampleHandler')
+	// 		.then((data: string) => {
+	// 			setBackendData(data)
+	// 		})
+	// }, [])
 
 	return (
 		<div className="oac-main-container">
 			Hello world!
-			{backendData && <strong>{backendData}</strong>}
 		</div>
 	)
 }
