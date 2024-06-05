cbd48cea0d0a31f44da3cd39b2139ae319eeb848
winstonji <winston.wy.ji@gmail.com>
2024-06-05 16:15:18 -0600

    working on edit lyric functionality. Unable to change <p> into <input> on click yet.

--- src/react/view/chartComponents/lyricSegmentComponent.tsx
+++ src/react/view/chartComponents/lyricSegmentComponent.tsx
@@ -2,7 +2,16 @@ import React, { useEffect, useState } from 'react'
 import { ChordWrapper } from '../../model/chordWrapper';
 
 function LyricSegmentComponent(chordWrapper:ChordWrapper) {
-	return (<p className='oac-lyric-segment'>
+
+	function handleClick(event){
+		console.log(event.target);
+		var inputField = document.createElement("input");
+		inputField.value = event.target.value;
+		event.target.parent.replaceChild(inputField, event.target);
+
+	}
+
+	return (<p className='oac-lyric-segment' onClick={handleClick}>
 		{chordWrapper.lyricSegment}
 	</p>);
 }
