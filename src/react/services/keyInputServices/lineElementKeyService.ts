b87355e0ed33565390052b325556a8ebafa1ad74
winstonji <112286408+winstonji@users.noreply.github.com>
2024-07-06 18:17:01 -0600

    refactor still not functional. LyricSegmentKeyDown and LineElementKeyDown done but unfunctional

--- src/react/services/keyInputServices/lineElementKeyService.ts
+++ src/react/services/keyInputServices/lineElementKeyService.ts
@@ -1,67 +1,104 @@
+import { Chart } from "../../model/chart";
+import { Line } from "../../model/line";
+import { LineElement } from "../../model/lineElement";
+import { FocusFinder } from "../../utils/focusFinderUtils";
+import { ChartEditingState } from "../../view/types/chartContext";
+import { CurrentFocus } from "../../view/types/currentFocus";
 
-} else if (event.key === 'ArrowRight' && (event.ctrlKey || cursorPosition === contentLength)) {
+export function handleLineElementKeyDown(
+    event: React.KeyboardEvent,
+    chartEditingState: ChartEditingState,
+    lineElement: LineElement,
+    cursorPosition: number,
+    contentLength: number
+): CurrentFocus {
+    if (event.key === 'ArrowRight' && (event.ctrlKey || cursorPosition === contentLength)) {
+        return handleArrowRight(event, lineElement);
+    } else if (event.key === 'ArrowLeft' && (event.ctrlKey || cursorPosition === 0)) {
+        return handleArrowLeft(event, lineElement);
+    } else if (event.key === 'ArrowUp') {
+        return handleArrowUp(event, lineElement);
+    } else if (event.key === 'ArrowDown') {
+        return handleArrowDown(event, lineElement);
+    } else if (event.code === 'Home') {
+        return handleHomeKey(event, chartEditingState.chart, lineElement);
+    } else if (event.code === 'End') {
+        return handleEndKey(event, chartEditingState.chart, lineElement);
+    }
+}
+
+function handleArrowRight(event:React.KeyboardEvent, lineElement:LineElement){
+    event.preventDefault();
     const newFocus = lineElement.getNext();
     if (newFocus) {
-        if (event.ctrlKey) {
-            setCurrentFocus({id: newFocus.lyricSegment.id, position: newFocus.lyricSegment.lyric.length});
-        } else {
-            setCurrentFocus({id: newFocus.lyricSegment.id, position: 0});
-        }
-        event.preventDefault();
+        return{id: newFocus.lyricSegment.id, position: 0};
     }
-} else if (event.key === 'ArrowLeft' && (event.ctrlKey || cursorPosition === 0)) {
-    const newFocus = lineElement.getPrevious();
+}
+
+function handleArrowLeft(event:React.KeyboardEvent, lineElement:LineElement){
+    event.preventDefault();
+    const newFocus = lineElement.getNext();
     if (newFocus) {
-        setCurrentFocus({id: newFocus.lyricSegment.id, position: newFocus.lyricSegment.lyric.length});
-        event.preventDefault();
-    } 
-} else if (event.key === 'ArrowUp') {
+        return{id: newFocus.lyricSegment.id, position: newFocus.lyricSegment.lyric.length};
+    }
+}
+
+function handleArrowUp(event:React.KeyboardEvent, lineElement:LineElement){
+    event.preventDefault();
     let newFocus:LineElement
     if (event.ctrlKey) {
         newFocus = FocusFinder.focusBoundExtremity(lineElement, lineElement.parent.parent, 'PREVIOUS');
-        if (newFocus) {
-            setCurrentFocus({id: newFocus.lyricSegment.id, position: newFocus.lyricSegment.lyric.length})
-        }
-        event.preventDefault();
-        return;
+    } else {
+        newFocus = FocusFinder.focusUpFrom(lineElement);
     }
-    newFocus = FocusFinder.focusUpFrom(lineElement);
-    if (newFocus) {
-        setCurrentFocus({id: newFocus.lyricSegment.id, position: newFocus.lyricSegment.lyric.length});
+
+    return {
+        id: newFocus.lyricSegment.id,
+        position: newFocus.lyricSegment.lyric.length
     }
+}
+
+function handleArrowDown(event:React.KeyboardEvent, lineElement:LineElement){
     event.preventDefault();
-} else if (event.key === 'ArrowDown') {
-    let newFocus:LineElement;
+    let newFocus:LineElement
     if (event.ctrlKey) {
         newFocus = FocusFinder.focusBoundExtremity(lineElement, lineElement.parent.parent, 'NEXT');
-        if (newFocus) {
-            setCurrentFocus({id: newFocus.lyricSegment.id, position: newFocus.lyricSegment.lyric.length})
-        }
-        event.preventDefault();
-        return;
+    } else {
+        newFocus = FocusFinder.focusDownFrom(lineElement);
     }
-    newFocus = FocusFinder.focusDownFrom(lineElement);
-    if (newFocus) {
-        setCurrentFocus({id: newFocus.lyricSegment.id, position: newFocus.lyricSegment.lyric.length});
+
+    return {
+        id: newFocus.lyricSegment.id,
+        position: newFocus.lyricSegment.lyric.length
     }
-    event.preventDefault();
-} else if (event.code === 'Home') {
+}
+
+function handleHomeKey(event:React.KeyboardEvent, chart:Chart, lineElement:LineElement){
     let newFocus:LineElement;
     if (event.ctrlKey) {
-        newFocus = FocusFinder.focusChartStart(chartEditingState.chart);
+        newFocus = FocusFinder.focusChartStart(chart);
     } else {
         newFocus = FocusFinder.focusBoundExtremity(lineElement, lineElement.parent, 'PREVIOUS_BOUNDED');
     }
     if (newFocus) {
-        setCurrentFocus({id: newFocus.lyricSegment.id, position: 0});
+        return {
+            id: newFocus.lyricSegment.id,
+            position: 0
+        }
     }
-} else if (event.code === 'End') {
+}
+
+function handleEndKey(event:React.KeyboardEvent, chart:Chart, lineElement:LineElement){
     let newFocus:LineElement;
     if (event.ctrlKey) {
-        newFocus = FocusFinder.focusChartEnd(chartEditingState.chart);
+        newFocus = FocusFinder.focusChartEnd(chart);
     } else {
         newFocus = FocusFinder.focusBoundExtremity(lineElement, lineElement.parent, 'NEXT_BOUNDED');
     }
     if (newFocus) {
-        setCurrentFocus({id: newFocus.lyricSegment.id, position: newFocus.lyricSegment.lyric.length});
+        return{
+            id: newFocus.lyricSegment.id,
+            position: newFocus.lyricSegment.lyric.length
+        }
     }
+}
--- src/react/services/keyInputServices/lyricSegmentKeyService.ts
+++ src/react/services/keyInputServices/lyricSegmentKeyService.ts
@@ -1,8 +1,10 @@
-import { Block } from "typescript";
 import { ChartService } from "../chartService";
 import { ChartEditingState } from "../../view/types/chartContext";
 import { LineElement } from "../../model/lineElement";
 import { Chart } from "../../model/chart";
+import { Block } from "../../model/block";
+import { Line } from "../../model/line";
+import { handleLineElementKeyDown } from "./lineElementKeyService";
 
 export function handleLyricSegmentKeyDown(
     event: React.KeyboardEvent,
@@ -12,118 +14,53 @@ export function handleLyricSegmentKeyDown(
     contentLength: number
 ): ChartEditingState{
     if (event.key === ' ') {
-        return handleSpace(chartEditingState.chart, lineElement, cursorPosition);
+        return handleSpace(
+            event,
+            chartEditingState.chart,
+            lineElement,
+            cursorPosition
+        );
     } else if (event.key === 'Enter') {
-        if (event.ctrlKey) {
-            setChartEditingState((chartEditingState) => {
-                const chartService = ChartService.with(chartEditingState.chart);
-                const newBlock:Block = chartService.insertNewBlockAfter(lineElement, cursorPosition);
-                return {
-                    chart: chartService.finalize(),
-                    currentFocus: {
-                        id: newBlock.children[0].children[0].lyricSegment.id,
-                        position: 0
-                    }
-                }
-            })
-        } else {
-            setChartEditingState((chartEditingState) => {
-                const chartService = ChartService.with(chartEditingState.chart);
-                const newLine = chartService.insertNewLineAfter(lineElement, cursorPosition);
-    
-                return {
-                    chart: chartService.finalize(),
-                    currentFocus: {
-                        id: newLine.children[0].lyricSegment.id,
-                        position: 0
-                    }
-                }
-            })
-        }
-        event.preventDefault();
+        return handleEnter(
+            event,
+            chartEditingState.chart,
+            lineElement,
+            cursorPosition
+        );
     } else if (event.key === 'Backspace' && cursorPosition === 0) {
-        event.preventDefault();
-        if (event.ctrlKey) {
-            setChartEditingState((chartEditingState) => {
-                const chartService = ChartService.with(chartEditingState.chart);
-                chartService.deletePrevious(lineElement);
-                return {
-                    chart: chartService.finalize(),
-                    currentFocus: {
-                        id: lineElement.lyricSegment.id,
-                        position: 0
-                    }
-                }
-            })
-            return;
-        } else if (lineElement.getPrevious() !== null){
-            setChartEditingState((chartEditingState) => {
-                const cursorPositionAfterMerge = lineElement.getPrevious().lyricSegment.lyric.length;
-                const chartService = ChartService.with(chartEditingState.chart);
-                const newFocus: LineElement = chartService.mergeLineElement(lineElement, -1);
-                const lineElementIndex:number = lineElement.parent.children.findIndex((element) => element.id === lineElement.id);
-                if (lineElementIndex === 0) {
-                    return {
-                        chart: chartService.finalize(),
-                        currentFocus: {
-                            id: newFocus.lyricSegment.id,
-                            position: 0
-                        }
-                    }
-                } else {
-                    return {
-                        chart: chartService.finalize(),
-                        currentFocus: {
-                            id: newFocus.lyricSegment.id,
-                            position: cursorPositionAfterMerge
-                        }
-                    }
-                }
-            });
-        }
+        return handleBackspace(
+            event,
+            chartEditingState.chart,
+            lineElement
+        );
     } else if (event.key === 'Delete' && cursorPosition === contentLength) {
-        event.preventDefault();
-        if (event.ctrlKey) {
-            setChartEditingState((chartEditingState) => {
-                const chartService = ChartService.with(chartEditingState.chart);
-                chartService.deleteNext(lineElement);
-                return {
-                    chart: chartService.finalize(),
-                    currentFocus: {
-                        id: lineElement.lyricSegment.id,
-                        position: contentLength
-                    }
-                }
-            })
-        } else {
-            setChartEditingState((chartEditingState) => {
-                const chartService = ChartService.with(chartEditingState.chart);
-                chartService.mergeLineElement(lineElement, 1);
-    
-                setCurrentFocus({})
-                return {
-                    chart: chartService.finalize(),
-                    currentFocus: {
-                        ...chartEditingState.currentFocus,
-                        position: contentLength
-                    }
-                }
-            });
-        }
-    
+        return handleDelete(
+            event,
+            chartEditingState,
+            lineElement,
+            contentLength
+        );
     } else if (event.ctrlKey && event.code === 'KeyK') {
-        event.preventDefault();
-        const chordSymbol = lineElement.chordSymbol;
-        console.log(`id: ${chordSymbol.id}`);
-        setCurrentFocus({id: chordSymbol.id, position:chordSymbol.backingString.length});
-    }
-    else{
-        return lineElementKeyService.processKeys();
+        return {
+            chart: {...chartEditingState.chart},
+            currentFocus: handleEditFocus(event, lineElement)
+        }
+    } else {
+        return {
+            chart: {...chartEditingState.chart},
+            currentFocus: handleLineElementKeyDown(
+                event,
+                chartEditingState,
+                lineElement,
+                cursorPosition,
+                contentLength
+            )
+        }
     }
     
 }
 
-function handleSpace(chart:Chart, lineElement, cursorPosition){
+function handleSpace(event:React.KeyboardEvent, chart:Chart, lineElement:LineElement, cursorPosition:number){
     event.preventDefault();
     const chartService = ChartService.with(chart);
     const newChordWrapper = chartService.splitLineElement(lineElement, '', cursorPosition);
@@ -134,18 +71,100 @@ function handleSpace(chart:Chart, lineElement, cursorPosition){
     }
 }
 
-function handleEnter(){
+function handleEnter(event:React.KeyboardEvent, chart:Chart, lineElement:LineElement, cursorPosition:number){
+    event.preventDefault();
+    if (event.ctrlKey) {
+        const chartService = ChartService.with(chart);
+        const newBlock:Block = chartService.insertNewBlockAfter(lineElement, cursorPosition);
+        return {
+            chart: chartService.finalize(),
+            currentFocus: {
+                id: newBlock.children[0].children[0].lyricSegment.id,
+                position: 0
+            }
+        }
+    } else {
+        const chartService = ChartService.with(chart);
+        const newLine = chartService.insertNewLineAfter(lineElement, cursorPosition);
 
+        return {
+            chart: chartService.finalize(),
+            currentFocus: {
+                id: newLine.children[0].lyricSegment.id,
+                position: 0
+            }
+        }
+    }
 }
 
-function handleBackspace(){
-
+function handleBackspace(event:React.KeyboardEvent, chart:Chart, lineElement:LineElement){
+    event.preventDefault();
+    if (event.ctrlKey) {
+        const chartService = ChartService.with(chart);
+        chartService.deletePrevious(lineElement);
+        return {
+            chart: chartService.finalize(),
+            currentFocus: {
+                id: lineElement.lyricSegment.id,
+                position: 0
+            }
+        }
+    } else if (lineElement.getPrevious() !== null){
+        const cursorPositionAfterMerge = lineElement.getPrevious().lyricSegment.lyric.length;
+        const chartService = ChartService.with(chart);
+        const newFocus: LineElement = chartService.mergeLineElement(lineElement, -1);
+        const lineElementIndex:number = lineElement.parent.children.findIndex((element) => element.id === lineElement.id);
+        if (lineElementIndex === 0) {
+            return {
+                chart: chartService.finalize(),
+                currentFocus: {
+                    id: newFocus.lyricSegment.id,
+                    position: 0
+                }
+            }
+        } else {
+            return {
+                chart: chartService.finalize(),
+                currentFocus: {
+                    id: newFocus.lyricSegment.id,
+                    position: cursorPositionAfterMerge
+                }
+            }
+        }
+    }
 }
 
-function handleDelete(){
-
+function handleDelete(event:React.KeyboardEvent, chartEditingState:ChartEditingState, lineElement:LineElement, contentLength:number){
+    event.preventDefault();
+    if (event.ctrlKey) {
+        const chartService = ChartService.with(chartEditingState.chart);
+        chartService.deleteNext(lineElement);
+        return {
+            chart: chartService.finalize(),
+            currentFocus: {
+                id: lineElement.lyricSegment.id,
+                position: contentLength
+            }
+        }
+    } else {
+        const chartService = ChartService.with(chartEditingState.chart);
+        chartService.mergeLineElement(lineElement, 1);
+        return {
+            chart: chartService.finalize(),
+            currentFocus: {
+                ...chartEditingState.currentFocus,
+                position: contentLength
+            }
+        }
+    }
 }
 
-function handleEditFocus(){
-
+function handleEditFocus(event:React.KeyboardEvent, lineElement:LineElement){
+    event.preventDefault();
+    const chordSymbol = lineElement.chordSymbol;
+    console.log(`id: ${chordSymbol.id}`);
+    return {
+        id: chordSymbol.id,
+        position:chordSymbol.backingString.length
+    };
 }
